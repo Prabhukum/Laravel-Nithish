aa8e3c3c9921c976a3bd19b8562620ca45308b85
RNKTamiz <118505055+RNKTamiz@users.noreply.github.com>
2023-04-21 11:48:00 +0530

    srgew

--- clonepage/resources/views/fourth.blade.php
+++ clonepage/resources/views/fourth.blade.php
@@ -2,16 +2,18 @@
 
 <style>
      input::placeholder {
-        font-weight: bold;
+        /* font-weight: bold; */
         opacity: 0.9;
-        color: black;
-        font-size:10px;
+        color: #A8A8A8;
+        font-size:14px;
 
     }
     a {
         text-decoration: none;
     }
-
+    input{
+        background: transparent;
+    }
     .card-outer{
         position: relative;
         top: 0px;
@@ -22,10 +24,11 @@
         margin-top: calc(calc(100vh - 934px) * 0.5);
         margin-left: calc(calc(100vw - 1366px) * 0.5);
     }
-    
+
 </style>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+
 <div class="card-outer">
     <div class="card shadow" >
         @include('includes.navbar')
@@ -51,25 +54,38 @@
                 <div class="container-fluid shadow p-2">
                     <span class="ms-1"style="font-size: 14px">search</span>
                     <div class="row ms-1 me-1 align-items-center" style="height: 69px; background: #EFF5FC">
-                            <div class="col-10">
+                        <form>
+                            {{-- <div class="col-10"> --}}
                                 <label style="font-size: 14px">Search by</label>
-                                <input type="date" class="ms-5" >
-                                <select  class="ms-4" aria-label="Default select example">
-                                    <option selected style="font-size: 14px">Borrower Name</option>
-                                    <option value="1" style="font-size: 14px">No Borrower Name</option>
-                                </select>
-                            </div>
-                            <div class="col-2">
-                                <button class="btnrst" style="width: 69px;  height: 28px;">Reset</button>
-                                <button class="btngo" style="width: 37px;  height: 28px;">Go</button>
-                            </div>
+                                <input list="brow" id="i" class="ms-4" placeholder="mm/dd/yy" autocomplete="off">
+                                    <datalist id="brow">
+                                        <option value="02/22/2023">
+                                        <option value="04/12/2023">
+                                        <option value="03/22/2023">
+                                        <option value="01/20/2023">
+                                        <option value="01/20/2023">
+                                    </datalist><img src="images/down-arrow-11.svg" alt="" style="width:10px;height:10px; margin-left:-15px;">
+                                    <input list="list" id="name" class="ms-4" placeholder="Borrower Name" autocomplete="off">
+                                    <datalist id="list">
+                                        <option value="Andy America">
+                                        <option value="Andy">
+                                        <option value="America">
+                                        <option value="Amy America">
+                                        <option value="Patricia Gail">
+                                    </datalist><img src="images/down-arrow-11.svg" alt="" style="width:10px;height:10px; margin-left:-15px;">
+                            {{-- </div>
+                            <div class="col-2"> --}}
+                                <button  class="btnrst clear-btn" style="width: 69px;  height: 28px; margin-left:415px;">Reset</button>
+                                <button  class="btngo" style="width: 37px;  height: 28px;">Go</button>
+                            {{-- </div> --}}
+                        </form>
                     </div>
                 </div>
 
             </div>
             <div class="px-3">
                 <div class="container-fluid shadow p-2" >
-                    <table id="example" class="table table-striped" style="width:100%">
+                    <table id="example" class="table " style="width:100%">
                         <thead style="background: #E3ECF6 0% 0% no-repeat padding-box;  font-size:12px">
                             <tr>
                                 <th>Scenario Name</th>
@@ -81,7 +97,8 @@
                                 <th>Action</th>
                             </tr>
                         </thead>
-                        <tbody style="border-radius:5px;background: #FCFCFC  0% 0% no-repeat padding-box; font-size:12px">
+
+                        <tbody id="body" style="border-radius:5px;background: #FCFCFC  0% 0% no-repeat padding-box; font-size:12px">
                             <tr>
                                 <td>Scenario-1</td>
                                 <td>Andy America</td>
@@ -127,7 +144,9 @@
                                 <td>Noki</td>
                                 <td>Awaiting Income</td>
                             </tr>
+                            
                         </tbody>
+
                     </table>
                 </div>
             </div>
@@ -139,15 +158,14 @@
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 
-
 <script src="assets/js/script.js"></script>
 <script>
     $(document).ready(function () {
-        $('#example').DataTable({
-            scrollY: '200px',
+       var dataTableInstance = $('#example').DataTable({
+            // scrollY: '200px',
             scrollCollapse: true,
             paging: true,
-            stripeClasses: [],
+
             pagingType: 'full_numbers',
 
             "language": {
@@ -178,8 +196,35 @@
 
         $(this).addClass("active2");
 
+    });
 
 
+
+
+
+
+    $(function() {
+        $('.btngo').on('click', function() {
+        event.preventDefault(); // prevent form submission
+        const input1 = $('#i').val();
+        const input2 = $('#name').val();
+        $('#example tbody tr').each(function() {
+          const column1Value = $(this).find('td:nth-child(3)').text();
+          const column2Value = $(this).find('td:nth-child(2)').text();
+          if ((input1 === '' || column1Value.includes(input1)) && (input2 === '' || column2Value.includes(input2))) {
+                $(this).show();
+            }
+
+          else {
+            $(this).hide(); // hide the row
+          }
+        });
+      });
+      $('button.clear-btn').click(function() {
+                $('#input1').val('');
+                $('#input2').val('');
+                $('#example tbody tr').show();
+        });
     });
 
 
