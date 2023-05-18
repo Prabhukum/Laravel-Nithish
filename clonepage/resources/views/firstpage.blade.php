84dcacc13db247f7e5967e6d73f5841cb4c9a388
PRABHUKUMAR J <nelsonprabhu07088@gmail.com>
2023-05-18 14:01:58 +0530

    A

--- clonepage/resources/views/firstpage.blade.php
+++ clonepage/resources/views/firstpage.blade.php
@@ -1,10 +1,9 @@
 @include('includes.header')
 
 <style>
-    @media (min-width: 768px) {
+    /* @media (min-width: 768px) {
         .form-check {
             display: inline-block;
-
             margin-bottom: 0;
         }
 
@@ -13,24 +12,18 @@
         }
     }
 
-
-
     @media (min-width: 1024px) and (max-height: 626px) {
-
+        .form-check-label {
+            white-space: normal;
+            display: inline-block;
+        }
         .form-check {
             flex-wrap: wrap;
             display: inline-block;
         }
-
         .form-check-input {
             margin-right: 0;
             margin-bottom: 5px;
-        }
-    }
-
-    @media (min-width: 1024px) and (max-height: 626px) {
-        .form-check-label {
-            white-space: normal;
         }
     }
 
@@ -39,18 +32,33 @@
         .form-check-label {
             font-size: 14px;
             white-space: nowrap;
+            display: inline-block;
+        }
+        .form-check {
+            flex-wrap: wrap;
+            display: inline-block;
         }
     }
 
     @media (max-width: 425px) {
         .form-check-label {
             font-size: 12px;
+            display: inline-block;
+        }
+        .form-check {
+            flex-wrap: wrap;
+            display: inline-block;
         }
     }
 
     @media (min-width: 992px) and (max-width: 1190px) {
         .form-check-label {
             font-size: 14px;
+            display: inline-block;
+        }
+        .form-check {
+            flex-wrap: wrap;
+            display: inline-block;
         }
 
         .form-check-inline {
@@ -62,45 +70,56 @@
             position: static;
             margin-right: 5px;
         }
+    } */
+    .cover_image{
+        /* margin-left:-12px;
+        width:100%; */
+      /* height:100%;
+    background-size:cover; */
+    height:100vh;
+    background-image:no-repeat;
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-image:url({{asset('images/Capture.PNG')}});
+ 
     }
-
+ 
 </style>
 
-<div class="container mt-4">
-    <div class="card-outer">
-        <div class="card shadow">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-8 ">
-                        <img src="images/Capture.PNG" alt="Glide Acra image" class="img-fluid d-none d-lg-block">
+<div class="container-fluid" style="height:100vh;">
+<!-- <div class="card-outer"> 
+       <div class="card shadow-none"> 
+          <div class="card-body">   -->
+                <div class="row" style="height:100vh;">
+                    <div class="col-lg-8 img-fluid cover_image d-none d-md-block d-lg-block d-inline-flex">
+                        <!-- <img src="images/Capture.PNG" style="height:100vh;" alt="Glide Acra image"  class="img-fluid cover_image d-none d-md-block d-lg-block d-inline-flex"> -->
                     </div>
-                    <div class="col-lg-4 align-self-center">
+                    <div class="col-lg-4 align-self-center"  style="">
                         <div class="text-center">
-                            <img src="images/header.PNG" alt="Glide Acra image" class="img-fluid mb-3"
-                                style="width: 187px; height: 74px;">
-                            <p class="mt-3"><b>Welcome to Acra Lending Broker Portal</b></p>
-                            <form class="px-5 mt-5">
+                            <img src="images/header.PNG" alt="Glide Acra image" class="img-fluid mb-3 mt-4" style="width: 187px; height: 74px;">
+                            <p class="mt-3"><b style="font-size: 13px;">Welcome to Acra Lending Broker Portal</b></p>
+                            <form class="px-lg-3 px-4 px-md-5 px-xxl-5 mt-5">
                                 <div class="mb-3">
-                                    <input type="text" name="name" placeholder="Email ID" class="form-control">
+                                    <input type="text" name="name" placeholder="Email ID" autocomplete="off" class="d-block mt-3 col-12">
                                 </div>
                                 <div class="mb-3">
-                                    <input type="password" name="password" placeholder="Password" class="form-control">
+                                    <input type="password" name="password" placeholder="Password" autocomplete="off" class=" d-block mt-3 col-12">
                                 </div>
                                 <div class="row mb-3">
-                                    <div class="col-6">
+                                    <div class="col-6 text-start">
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox" id="inlineFormCheck">
-                                            <label class="form-check-label" for="inlineFormCheck">Remember me</label>
+                                            <label class="form-check-label" style="font-size: 10px;" for="inlineFormCheck"><b>Remember me</b></label>
                                         </div>
                                     </div>
                                     <div class="col-6 text-end">
-                                        <a href="#" style="font-size: 12px; color: #0033A1;">Forgot Your Password?</a>
+                                        <a href="#" style="font-size: 9px; color: #0033A1; text-decoration:none;"><b>Forgot Your Password?</b></a>
                                     </div>
                                 </div>
-                                <div class="text-center mt-4">
-                                    <div class="d-flex justify-content-center">
-                                        <a  href="/second" class="btn btn-primary text-white"
-                                            style="background: #213E97; width: 345px; height: 48px; border-radius: 5px; color: white;"><b>NEXT</b></a>
+                                <div class="mt-4  mb-5">
+                                    <div class="d-flex justify-content-center ">
+                                        <a  href="/second" class=" btn btn-primary "
+                                            style="background: #213E97; width: 300px; height:40px; margin-bottom: 12px; border-radius: 5px; color: white;"><b class="text-white">NEXT</b></a>
                                     </div>
                                 </div>
 
@@ -108,9 +127,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
+           </div>
         </div>
-    </div>
+    </div> 
 </div>
 
 @include('includes.footer')
